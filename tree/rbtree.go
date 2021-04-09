45ec844e7bab56c659e1edb3e18cc846e15fb34a
nireo <eemil.lehtikangas1@gmail.com>
2021-04-09 14:51:45 +0300

    added proper insertions and lookup

--- tree/rbtree.go
+++ tree/rbtree.go
@@ -63,5 +63,158 @@ func (tree *Tree) Insert(key, val []byte) {
 		}
 		insertNode.Parent = node
 	}
+
+	tree.insert1(insertNode)
 	tree.size++
+}
+
+func (tree *Tree) Get(key []byte) (value []byte, ok bool) {
+	node := tree.find(key)
+	if node != nil {
+		return node.Value, true
+	}
+
+	return nil, false
+}
+
+func (tree *Tree) find(key []byte) *Node {
+	node := tree.Root
+	for node != nil {
+		comp := bytes.Compare(key, node.Key)
+		switch {
+		case comp == 0:
+			return node
+		case comp < 0:
+			node = node.Left
+		case comp < 0:
+			node = node.Right
+		}
+	}
+
+	return nil
+}
+
+func (tree *Tree) insert1(node *Node) {
+	if node.Parent == nil {
+		node.Color = black
+	} else {
+		tree.insert2(node)
+	}
+}
+
+func (tree *Tree) insert2(node *Node) {
+	if getColor(node.Parent) == black {
+		return
+	}
+
+	tree.insert3(node)
+}
+
+func (tree *Tree) insert3(node *Node) {
+	uncle := node.uncle()
+	if getColor(uncle) == red {
+		node.Parent.Color = black
+		uncle.Color = black
+		node.grandparent().Color = red
+		tree.insert1(node.grandparent())
+	} else {
+		tree.insert4(node)
+	}
+}
+
+func (tree *Tree) insert4(node *Node) {
+	grandparent := node.grandparent()
+	if node == node.Parent.Right && node.Parent == grandparent.Left {
+		tree.lRotate(node.Parent)
+		node = node.Left
+	} else if node == node.Parent.Left && node.Parent == grandparent.Right {
+		tree.rRotate(node.Parent)
+		node = node.Right
+	}
+
+	tree.insert5(node)
+}
+
+func (tree *Tree) insert5(node *Node) {
+	node.Parent.Color = black
+	grandparent := node.grandparent()
+	grandparent.Color = red
+
+	if node == node.Parent.Left && node.Parent == grandparent.Left {
+		tree.rRotate(grandparent)
+	} else if node == node.Parent.Right && node.Parent == grandparent.Right {
+		tree.lRotate(grandparent)
+	}
+}
+
+func (node *Node) uncle() *Node {
+	if node == nil || node.Parent == nil || node.Parent.Parent == nil {
+		return nil
+	}
+
+	return node.Parent.sibling()
+}
+
+func (node *Node) sibling() *Node {
+	if node == nil || node.Parent == nil {
+		return nil
+	}
+
+	if node == node.Parent.Left {
+		return node.Parent.Right
+	}
+
+	return node.Parent.Left
+}
+
+func (node *Node) grandparent() *Node {
+	if node != nil && node.Parent != nil {
+		return node.Parent.Parent
+	}
+	return nil
+}
+
+func getColor(node *Node) nodeColor {
+	if node == nil {
+		return black
+	}
+
+	return node.Color
+}
+
+func (tree *Tree) lRotate(node *Node) {
+	right := node.Right
+	tree.replace(node, right)
+	node.Right = right.Left
+	if right.Left != nil {
+		right.Left.Parent = node
+	}
+	right.Left = node
+	node.Parent = right
+}
+
+func (tree *Tree) rRotate(node *Node) {
+	left := node.Left
+	tree.replace(node, left)
+	node.Left = left.Right
+	if left.Right != nil {
+		left.Right.Parent = node
+	}
+	left.Right = node
+	node.Parent = left
+}
+
+func (tree *Tree) replace(old *Node, new *Node) {
+	if old.Parent == nil {
+		tree.Root = new
+	} else {
+		if old == old.Parent.Left {
+			old.Parent.Left = new
+		} else {
+			old.Parent.Right = new
+		}
+	}
+	if new != nil {
+		new.Parent = old.Parent
+	}
 }
