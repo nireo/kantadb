e36be4557ca2e479ae1bd2f62213f70961e1972c
nireo <eemil.lehtikangas1@gmail.com>
2021-04-18 15:13:07 +0300

    started working on a in-memory table with a max size and using a tree

--- mem/mem.go
+++ mem/mem.go
@@ -1,6 +1,7 @@
 package mem
 
 import (
+	"encoding/binary"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -8,6 +9,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/emirpasic/gods/trees/redblacktree"
 	"github.com/nireo/kantadb/entries"
 	"github.com/nireo/kantadb/utils"
 )
@@ -147,4 +149,70 @@ func (m *MEM) WriteToLog(key, val string) {
 // DeleteLogFile deletes the log file after all of the values have been stored into an sstable
 func (m *MEM) DeleteLogFile() error {
 	return os.Remove(m.logFilePath)
+}
+
+// MaxMEM contains a binary tree with a max capacity
+type MaxMEM struct {
+	Tree    *redblacktree.Tree
+	Size    int64
+	wrMutex sync.RWMutex
+	lgMutex sync.Mutex
+	lgFile  *os.File
+}
+
+// NewMaxMEM creates a memtable with a max capacity
+func NewMaxMEM() *MaxMEM {
+	timestamp := time.Now().UnixNano()
+	filePath := filepath.Join(logPath, fmt.Sprintf("%v.lg", timestamp))
+
+	// create the new file
+	file, err := os.Create(filePath)
+	if err != nil {
+		// error happened skip this and try again on the next iteration
+		utils.PrintDebug("error creating logfile: %s", err)
+	}
+
+	return &MaxMEM{
+		Tree:    redblacktree.NewWithStringComparator(),
+		Size:    0,
+		lgFile:  file,
+		lgMutex: sync.Mutex{},
+		wrMutex: sync.RWMutex{},
+	}
+}
+
+// Put places a key into the in-memory tree and then updates the size of the tree.
+func (mx *MaxMEM) Put(key, val string) {
+	mx.wrMutex.Lock()
+	defer mx.wrMutex.Unlock()
+
+	mx.Tree.Put(key, val)
+	klenBuffer := make([]byte, 4)
+	vlenBuffer := make([]byte, 4)
+	binary.BigEndian.PutUint32(klenBuffer, uint32(len([]byte(key))))
+	binary.BigEndian.PutUint32(vlenBuffer, uint32(len([]byte(val))))
+
+	data := make([]byte, 1)
+	data = append(data, klenBuffer...)
+	data = append(data, vlenBuffer...)
+	data = append(data, []byte(key)...)
+	data = append(data, []byte(val)...)
+
+	mx.Size += int64(len(data))
+
+	mx.lgFile.Write(data)
+}
+
+// Get finds a value from the in-memory table
+func (mx *MaxMEM) Get(key string) (string, bool) {
+	mx.wrMutex.RLock()
+	val, ok := mx.Tree.Get("")
+	mx.wrMutex.RUnlock()
+
+	return fmt.Sprintf("%v", val), ok
+}
+
+// Close closes the log file connection
+func (mx *MaxMEM) Close() error {
+	return mx.lgFile.Close()
 }
